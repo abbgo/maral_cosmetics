a277280c7517dd385a733306308defa3bb081136
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 11:29:54 +0500

    feat: add shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:maral_cosmetics/pages/home.dart';
+import 'package:maral_cosmetics/providers/local_storadge.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance(); // shared preferences
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
