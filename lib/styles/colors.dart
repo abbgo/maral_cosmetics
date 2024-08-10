e13ba60ee83da3064d2f8b244c71b04097faed46
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 11:41:57 +0500

    feat: add defaultLightColor varibale

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 
 Color defaultColor = const Color(0xffa16f8a);
-Color scaffoldColorDarkTheme = const Color(0xff212121);
-Color dialogColorDarkTheme = const Color(0xff424242);
+Color defaultLightColor = const Color(0xfff6f1f4);
