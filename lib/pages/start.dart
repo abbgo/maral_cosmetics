6d8e48ed380f4f9af024e08f89b052d983efdae5
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 12:04:47 +0500

    feat: add goToPage method and add StartPage

---
+++ lib/pages/start.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/methods/navigation.dart';
+import 'package:maral_cosmetics/pages/home.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  // INIT STATE ----------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+    _moveNextPage();
+  }
+
+  _moveNextPage() async {
+    await Future.delayed(const Duration(seconds: 1));
+    if (mounted) {
+      goToPage(context, const HomePage(), true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo.jpg"),
+            Image.asset("assets/animated_icons/loading.gif", fit: BoxFit.cover),
+          ],
+        ),
+      ),
+    );
+  }
+}
