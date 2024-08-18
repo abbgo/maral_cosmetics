67996c5d60073e3c2f96c10450b24156240a4746
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 10:38:17 +0500

    fix: update HomeDiscountProductsBody part widget

--- lib/pages/parts/home_discount_products/parts/home_discount_products_body.dart
+++ lib/pages/parts/home_discount_products/parts/home_discount_products_body.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
 
 class HomeDiscountProductsBody extends StatelessWidget {
   const HomeDiscountProductsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Body');
+    return Container(
+      height: 430,
+      decoration: BoxDecoration(
+        color: defaultLightColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 8),
+            child: Text(
+              'Bayramcylyk mynasybetli Arzanladyslar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: defaultColor,
+                fontFamily: 'Niconne',
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
