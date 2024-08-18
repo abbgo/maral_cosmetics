321b0a973cd0509f061e45dee3d83b44d4721a6a
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 12:54:40 +0500

    fix: update HomeNewsMainImage part widget

--- lib/pages/parts/home_news/parts/home_news_main_image.dart
+++ lib/pages/parts/home_news/parts/home_news_main_image.dart
@@ -8,10 +8,36 @@ class HomeNewsMainImage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsetsDirectional.symmetric(horizontal: 10),
       child: Stack(
+        alignment: const Alignment(-0.85, 0.85),
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image.asset('assets/examples/news_main.png'),
+          ),
+          Container(
+            width: 175,
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white.withOpacity(.5),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Customers Says about our Servise',
+                  style: TextStyle(fontSize: 14),
+                ),
+                Text(
+                  '04.08.2024',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'HeyWowRegular',
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
