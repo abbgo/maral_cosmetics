d4cd3cd1932803b892d8d18ab1485be59ac4e633
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 10:16:52 +0500

    feat: add HomeDiscountProducts part widget

--- lib/pages/home/parts/home_body.dart
+++ lib/pages/home/parts/home_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/parts/home_discount_products/home_discount_products.dart';
 import 'package:maral_cosmetics/pages/parts/home_new_brends_slider/home_new_brends_slider.dart';
 import 'package:maral_cosmetics/pages/home/parts/home_slider.dart';
 import 'package:maral_cosmetics/pages/parts/home_menus/home_menus.dart';
@@ -16,6 +17,8 @@ class HomeBody extends StatelessWidget {
           HomeSlider(),
           SizedBox(height: 20),
           HomeNewBrendsSlider(),
+          SizedBox(height: 20),
+          HomeDiscountProducts(),
         ],
       ),
     );
