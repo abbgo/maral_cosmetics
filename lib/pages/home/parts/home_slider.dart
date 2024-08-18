0585d35d5df792068e3573560e5274de0a29f7c9
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 08:16:59 +0500

    fix: update HomeSlider part widget

--- lib/pages/home/parts/home_slider.dart
+++ lib/pages/home/parts/home_slider.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/static_variables/static_variables.dart';
 
-class HomeSlider extends StatelessWidget {
+class HomeSlider extends StatefulWidget {
   const HomeSlider({super.key});
 
   @override
+  State<HomeSlider> createState() => _HomeSliderState();
+}
+
+class _HomeSliderState extends State<HomeSlider> {
+  final PageController _pageController = PageController(viewportFraction: 1.05);
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Slider'),
+    return SizedBox(
+      height: 200,
+      child: PageView.builder(
+        controller: _pageController,
+        pageSnapping: true,
+        itemCount: homeSliders.length,
+        itemBuilder: (context, index) => FractionallySizedBox(
+          widthFactor: 1 / _pageController.viewportFraction,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(homeSliders[index], fit: BoxFit.cover),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/static_variables/static_variables.dart
+++ lib/static_variables/static_variables.dart
@@ -1,1 +1,6 @@
 List<String> homeMenus = ["Esasy", "Kategoriya", "Brendler", "Toplum"];
+List<String> homeSliders = [
+  "assets/examples/slider_1.jpg",
+  "assets/examples/slider_2.png",
+  "assets/examples/slider_3.png",
+];
