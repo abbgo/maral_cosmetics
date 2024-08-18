4393411742ef7d7db082acae39aef9eb6724994a
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 11:17:09 +0500

    feat: add HomeDiscountProductCard part widget

---
+++ lib/pages/parts/home_discount_products_list/parts/home_discount_product_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeDiscountProductCard extends StatelessWidget {
+  const HomeDiscountProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Haryt');
+  }
+}
--- lib/pages/parts/home_discount_products_list/parts/home_discount_products.dart
+++ lib/pages/parts/home_discount_products_list/parts/home_discount_products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/parts/home_discount_products_list/parts/home_discount_product_card.dart';
 
 class HomeDiscountProducts extends StatelessWidget {
   const HomeDiscountProducts({super.key});
@@ -9,7 +10,7 @@ class HomeDiscountProducts extends StatelessWidget {
       height: 235,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => Text('Haryt'),
+        itemBuilder: (context, index) => const HomeDiscountProductCard(),
         separatorBuilder: (context, index) => const SizedBox(width: 10),
         itemCount: 3,
       ),
