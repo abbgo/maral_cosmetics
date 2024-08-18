e0571818c0831d2045a3608acb0bee3bd11b933a
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-18 09:52:47 +0500

    feat: add HomeNewBrendsSlider part widget

--- lib/pages/home/parts/home_body.dart
+++ lib/pages/home/parts/home_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/home/parts/home_new_brends_slider.dart';
 import 'package:maral_cosmetics/pages/home/parts/home_slider.dart';
 import 'package:maral_cosmetics/pages/parts/home_menus/home_menus.dart';
 
@@ -13,6 +14,7 @@ class HomeBody extends StatelessWidget {
         children: const [
           HomeMenus(),
           HomeSlider(),
+          HomeNewBrendsSlider(),
         ],
       ),
     );
