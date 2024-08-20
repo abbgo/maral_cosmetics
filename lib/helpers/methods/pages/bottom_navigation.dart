00877968cf89851a10050faf7a84939991d87fc1
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-08-20 20:37:34 +0500

    fix: update bottomNavigationBarItemMethod method

--- lib/helpers/methods/pages/bottom_navigation.dart
+++ lib/helpers/methods/pages/bottom_navigation.dart
@@ -10,10 +10,9 @@ BottomNavigationBarItem bottomNavigationBarItemMethod(
       mainAxisSize: MainAxisSize.min,
       children: [
         Icon(icon, size: 24),
-        Text(
-          isSelected ? '.' : '',
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-        ),
+        isSelected
+            ? Image.asset('assets/images/point.png')
+            : const SizedBox(height: 4),
       ],
     ),
     backgroundColor: const Color(0xffF1E0E9),
