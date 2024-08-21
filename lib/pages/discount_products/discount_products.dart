c346715d86f9d07b1772da0121f9ec828da37d50
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-21 05:28:16 +0500

    feat: add DiscountProductsPage

---
+++ lib/pages/discount_products/discount_products.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DiscountProductsPage extends StatelessWidget {
+  const DiscountProductsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Arzanladyslar'),
+      ),
+      body: const Center(
+        child: Text("body"),
+      ),
+    );
+  }
+}
--- lib/pages/parts/home_discount_products_list/home_discount_products_list.dart
+++ lib/pages/parts/home_discount_products_list/home_discount_products_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/methods/navigation.dart';
+import 'package:maral_cosmetics/pages/discount_products/discount_products.dart';
 import 'package:maral_cosmetics/pages/parts/home_discount_products_list/parts/home_discount_products_body.dart';
 import 'package:maral_cosmetics/pages/parts/home_new_brends_slider/parts/home_new_brends_slider_title.dart';
 
@@ -12,7 +14,16 @@ class HomeDiscountProductsList extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          HomeNewBrendsSliderTitle(text: 'Arzanladyslar', onTap: () {}),
+          HomeNewBrendsSliderTitle(
+            text: 'Arzanladyslar',
+            onTap: () => Navigator.push(
+              context,
+              CustomPageRoute(
+                child: const DiscountProductsPage(),
+                direction: AxisDirection.left,
+              ),
+            ),
+          ),
           const HomeDiscountProductsBody(),
         ],
       ),
