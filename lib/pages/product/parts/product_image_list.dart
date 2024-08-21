d674a1bf3a68f4cc21624893e8d3439a2f951d59
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-21 13:05:51 +0500

    fix: update ProductImageList part widget

--- lib/pages/product/parts/product_image_list.dart
+++ lib/pages/product/parts/product_image_list.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/examples/static_variables.dart';
+import 'package:maral_cosmetics/providers/pages/product.dart';
 
-class ProductImageList extends StatelessWidget {
+class ProductImageList extends ConsumerWidget {
   const ProductImageList({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15),
       child: SizedBox(
         height: 85,
         child: ListView.separated(
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => ClipRRect(
-            borderRadius: BorderRadius.circular(5),
-            child: Image.asset(
-              productSliders[index],
-              width: 125,
-              fit: BoxFit.cover,
+          itemBuilder: (context, index) => GestureDetector(
+            onTap: () =>
+                ref.read(selectedProductImageProvider.notifier).state = index,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Image.asset(
+                productSliders[index],
+                width: 125,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           separatorBuilder: (context, index) => const SizedBox(width: 5),
