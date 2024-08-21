977a55caaa531b0c5413bcc3575a272c85a7bfdf
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-21 14:29:41 +0500

    fix: update ProductDescription part widget

--- lib/pages/product/parts/product_description.dart
+++ lib/pages/product/parts/product_description.dart
@@ -7,6 +7,13 @@ class ProductDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      iconColor: defaultColor,
+      collapsedIconColor: defaultColor,
+      backgroundColor: Colors.blueGrey.withOpacity(.1),
+      childrenPadding: const EdgeInsets.all(20),
       title: Text(
         'Dusundirilisi',
         style: TextStyle(
@@ -15,12 +22,9 @@ class ProductDescription extends StatelessWidget {
         ),
       ),
       children: const [
-        Padding(
-          padding: EdgeInsets.all(20),
-          child: Text(
-            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. ',
-            style: TextStyle(fontFamily: 'HeyWowRegular'),
-          ),
+        Text(
+          'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. ',
+          style: TextStyle(fontFamily: 'HeyWowRegular'),
         )
       ],
     );
