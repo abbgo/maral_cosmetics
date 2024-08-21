7257c6d1e99ec39d154a3b2833ccbd370bc9e725
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-21 12:30:36 +0500

    fix: update ProductPageAppBar part widget

--- lib/pages/product/parts/product_page_app_bar.dart
+++ lib/pages/product/parts/product_page_app_bar.dart
@@ -6,25 +6,28 @@ class ProductPageAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              color: defaultLightColor,
-              borderRadius: BorderRadius.circular(5),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: defaultLightColor,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Icon(Icons.close, color: defaultColor),
             ),
-            child: Icon(Icons.close, color: defaultColor),
           ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.share_outlined, color: defaultColor),
-        ),
-      ],
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.share_outlined, color: defaultColor),
+          ),
+        ],
+      ),
     );
   }
 }
