131ed9933c7bd38a56bfc4771da1fbc247cdd36d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-21 06:46:35 +0500

    fix: update HomeAboutCosmetics part widget

--- lib/pages/parts/home_about_cosmetics/home_about_cosmetics.dart
+++ lib/pages/parts/home_about_cosmetics/home_about_cosmetics.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/pages/parts/home_about_cosmetics/parts/home_about_cosmetics_list.dart';
-import 'package:maral_cosmetics/pages/parts/home_new_brends_slider/parts/home_new_brends_slider_title.dart';
 
 class HomeAboutCosmetics extends StatelessWidget {
   const HomeAboutCosmetics({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        HomeNewBrendsSliderTitle(text: 'Kosmetika barada', onTap: () {}),
-        const HomeAboutCosmeticsList(),
+        ListTile(
+          title: Text('Kosmetika barada', style: TextStyle(fontSize: 20)),
+        ),
+        HomeAboutCosmeticsList(),
       ],
     );
   }
