4f6b99af6c2142c8628d05a57417c2621e71c5b6
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-22 10:49:26 +0500

    fix: update FavoriteProductCard part widget

--- lib/pages/favorites/parts/favorite_product_card.dart
+++ lib/pages/favorites/parts/favorite_product_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/methods/navigation.dart';
 import 'package:maral_cosmetics/pages/favorites/parts/favorite_product_data.dart';
 import 'package:maral_cosmetics/pages/favorites/parts/favorite_product_images_slider.dart';
+import 'package:maral_cosmetics/pages/product/product.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
 
 class FavoriteProductCard extends StatelessWidget {
@@ -10,18 +12,21 @@ class FavoriteProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: defaultLightColor.withOpacity(.2),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          FavoriteProductImagesSlider(images: images),
-          const SizedBox(width: 10),
-          const FavoriteProductData(),
-        ],
+    return GestureDetector(
+      onTap: () => goToPage(context, const ProductPage(), false),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: defaultLightColor.withOpacity(.2),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            FavoriteProductImagesSlider(images: images),
+            const SizedBox(width: 10),
+            const FavoriteProductData(),
+          ],
+        ),
       ),
     );
   }
