ad74456dcfab36285a75512f6211656bfbf73cf0
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-22 11:39:20 +0500

    fix: update BasketProductButtons part widget

--- lib/pages/basket/parts/basket_product_buttons.dart
+++ lib/pages/basket/parts/basket_product_buttons.dart
@@ -10,46 +10,33 @@ class BasketProductButtons extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Icon(
-              Icons.star,
-              size: 12,
-              color: Color(0xffFFBB00),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.remove, color: defaultColor),
             ),
-            const SizedBox(width: 2),
             Text(
-              '3.0',
+              '10',
               style: TextStyle(
-                fontSize: 10,
+                fontSize: 16,
                 fontFamily: 'HeyWowRegular',
                 color: defaultColor,
               ),
             ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add, color: defaultColor),
+            ),
           ],
         ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-              decoration: BoxDecoration(
-                color: defaultLightColor.withOpacity(.8),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Icon(Icons.local_mall_outlined, color: defaultColor),
-            ),
-            const SizedBox(width: 10),
-            Container(
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: defaultLightColor.withOpacity(.8),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Icon(Icons.favorite, color: defaultColor),
-            ),
-          ],
+        Container(
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCBCC),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: const Icon(Icons.close, color: Colors.red),
         ),
       ],
     );
