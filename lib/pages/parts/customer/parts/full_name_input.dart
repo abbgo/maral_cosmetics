08c461605bbf8e352ec8adc58bdb540e9fe52649
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-23 08:08:30 +0500

    fix: update PhoneNumberInput part widget

--- lib/pages/parts/customer/parts/full_name_input.dart
+++ lib/pages/parts/customer/parts/full_name_input.dart
@@ -7,7 +7,7 @@ class FullNameInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: TextFormField(
         cursorColor: defaultColor,
         keyboardType: TextInputType.text,
--- lib/pages/parts/customer/parts/phone_number_input.dart
+++ lib/pages/parts/customer/parts/phone_number_input.dart
@@ -7,11 +7,26 @@ class PhoneNumberInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: TextFormField(
         cursorColor: defaultColor,
         keyboardType: TextInputType.text,
-        decoration: const InputDecoration(labelText: ' Doly Adynyz '),
+        decoration: InputDecoration(
+          prefixIcon: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  ' +993 | ',
+                  style: TextStyle(
+                      color: defaultColor, fontFamily: 'HeyWowRegular'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
