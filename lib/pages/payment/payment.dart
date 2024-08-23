128b027bec5849d9f64552bfece146f169e8df01
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-23 07:02:06 +0500

    feat: add PaymentType part widget

--- lib/pages/payment/payment.dart
+++ lib/pages/payment/payment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/payment/parts/payment_type.dart';
 
 class PaymentPage extends StatelessWidget {
   const PaymentPage({super.key});
@@ -10,8 +11,13 @@ class PaymentPage extends StatelessWidget {
         centerTitle: false,
         title: const Text('Toleg'),
       ),
-      body: const Center(
-        child: Text('Toleg body'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: ListView(
+          children: const [
+            PaymentType(),
+          ],
+        ),
       ),
     );
   }
