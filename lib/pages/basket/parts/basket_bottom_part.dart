10acecce778ccdaa3d53f12b422208a61e3b9215
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-23 06:44:42 +0500

    fix: update BasketBottomPart part widget

--- lib/pages/basket/parts/basket_bottom_part.dart
+++ lib/pages/basket/parts/basket_bottom_part.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
 
 class BasketBottomPart extends StatelessWidget {
   const BasketBottomPart({super.key});
@@ -13,12 +14,46 @@ class BasketBottomPart extends StatelessWidget {
           horizontal: BorderSide(color: Color(0xffF1E0E9), width: 1),
         ),
       ),
-      child: const Column(
+      child: Column(
         children: [
-          Text(
+          const Text(
             'Minimum sargyt 10 manat.Sargyt etmek ucin yene 30 manat gosmaly.',
             style: TextStyle(fontFamily: 'HeyWowRegular'),
-          )
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: const Color(0xffF1E0E9),
+                    child: Icon(Icons.local_mall_outlined, color: defaultColor),
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Jemi: 34.50 m',
+                    style: TextStyle(
+                      fontFamily: 'HeyWowRegular',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: defaultColor),
+                onPressed: () {},
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Dowam et'),
+                    SizedBox(width: 10),
+                    Icon(Icons.arrow_forward, size: 20),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
