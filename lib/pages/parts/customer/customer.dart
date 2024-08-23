33f8371ff83495888359ffd9e56e6392144bd33f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-23 07:32:50 +0500

    feat: add CustomerPart part widget

---
+++ lib/pages/parts/customer/customer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomerPart extends StatelessWidget {
+  const CustomerPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
