618cecf7fd01dcbf64763fbcbf9f610cec5a5bf0
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-24 11:29:58 +0500

    feat: add LanguageListTile part widget and update SelectLanguage part widget

---
+++ lib/pages/parts/select_language/parts/language_list_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
+
+class LanguageListTile extends StatelessWidget {
+  const LanguageListTile({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        text,
+        style: TextStyle(fontFamily: 'HeyWowRegular', color: defaultColor),
+      ),
+      trailing: Icon(Icons.check, color: defaultColor),
+    );
+  }
+}
--- lib/pages/parts/select_language/select_language.dart
+++ lib/pages/parts/select_language/select_language.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/parts/select_language/parts/language_list_tile.dart';
 
 class SelectLanguage extends StatelessWidget {
   const SelectLanguage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            width: 40,
+            height: 4,
+            decoration: BoxDecoration(
+              color: const Color(0xff824D6A),
+              borderRadius: BorderRadius.circular(60),
+            ),
+          ),
+          const LanguageListTile(text: 'Turkmen dili'),
+          const Divider(color: Color(0xffD9D9D9)),
+          const LanguageListTile(text: 'Rus dili'),
+        ],
+      ),
+    );
   }
 }
