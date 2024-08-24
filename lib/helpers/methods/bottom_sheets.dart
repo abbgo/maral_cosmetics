7b7b5da7465d737fa81b97a1aece368ac6e977c3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-24 11:57:40 +0500

    feat: add LogOutBottomSheet part widget

--- lib/helpers/methods/bottom_sheets.dart
+++ lib/helpers/methods/bottom_sheets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/pages/parts/contact_phones/contact_phones.dart';
+import 'package:maral_cosmetics/pages/parts/log_out_bottom_sheet/log_out_bottom_sheet.dart';
 import 'package:maral_cosmetics/pages/parts/select_language/select_language.dart';
 
 showLanguageBottomSheet(BuildContext context) {
@@ -19,6 +20,6 @@ showPhonesBottomSheet(BuildContext context) {
 showLogOutBottomSheet(BuildContext context) {
   showModalBottomSheet(
     context: context,
-    builder: (context) => const ContactPhones(),
+    builder: (context) => const LogOutBottomSheet(),
   );
 }
