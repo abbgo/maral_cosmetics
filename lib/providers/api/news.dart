25f1ce3d3beb5ed0a8fe71f454a4a4d65da63a95
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-02 15:06:32 +0500

    feat: add fetchNewsProvider provider

--- lib/providers/api/news.dart
+++ lib/providers/api/news.dart
@@ -1,22 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/models/news.dart';
+import 'package:maral_cosmetics/providers/pages/news.dart';
 import 'package:maral_cosmetics/services/api/news.dart';
 
 final newsApiProvider = Provider<NewsApiService>((ref) => NewsApiService());
 
-var fetchNewsProvider = FutureProvider.family<ResultNews, int>(
+var fetchNewsProvider = FutureProvider.autoDispose.family<ResultNews, int>(
   (ref, arg) async {
     ResultNews result = ResultNews.defaultResult();
 
     try {
-      String search = ref.watch(shopSearchProvider);
+      String search = await ref.watch(newsSearchProvider);
 
       List<NewsModel> newss =
           await ref.read(newsApiProvider).fetchNews(page: arg, search: search);
 
-      // if (search != '') {
-      //   ref.read(hasShopsProvider.notifier).state = shops.isNotEmpty;
-      // }
+      if (search != '') {
+        ref.read(hasNewsProvider.notifier).state = newss.isNotEmpty;
+      }
 
       result = ResultNews(newss: newss, error: '');
     } catch (e) {
