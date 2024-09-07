5912d85f500188e9ea8032f2ccc598bf49bbf6ef
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-07 16:22:24 +0500

    feat: add fetchBrandsProvider , brandSearchProvider , hasBrandsProvider , loadBrandProvider provdirs

--- lib/providers/api/brand.dart
+++ lib/providers/api/brand.dart
@@ -1,4 +1,35 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/models/brand.dart';
+import 'package:maral_cosmetics/models/default_param.dart';
+import 'package:maral_cosmetics/providers/pages/brands.dart';
 import 'package:maral_cosmetics/services/api/brand.dart';
 
 final brandApiProvider = Provider<BrandApiService>((ref) => BrandApiService());
+
+var fetchBrandsProvider = FutureProvider.family<ResultBrands, DefaultParams>(
+  (ref, arg) async {
+    ResultBrands result = ResultBrands.defaultResult();
+
+    try {
+      String search = await ref.watch(brandSearchProvider);
+
+      ResultBrands resultBrands = await ref.read(brandApiProvider).fetchBrands(
+            page: arg.page!,
+            pageSize: arg.pageSize!,
+            search: search,
+          );
+
+      if (search != '') {
+        ref.read(hasBrandsProvider.notifier).state =
+            resultBrands.brands!.isNotEmpty;
+      }
+
+      result = resultBrands;
+    } catch (e) {
+      result = ResultBrands(error: e.toString());
+    }
+
+    ref.read(loadBrandProvider.notifier).state = false;
+    return result;
+  },
+);
---
+++ lib/providers/pages/brands.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+var brandSearchProvider = StateProvider<String>((ref) => '');
+var hasBrandsProvider = StateProvider.autoDispose<bool>((ref) => true);
+final loadBrandProvider = StateProvider.autoDispose<bool>((ref) => true);
