0477563d34be41cdb7b3e29c8b7836dba22c23e6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-07 16:17:58 +0500

    feat: add fetchBrands function

--- lib/services/api/brand.dart
+++ lib/services/api/brand.dart
@@ -1,1 +1,51 @@
-class BrandApiService {}
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:maral_cosmetics/helpers/static_data.dart';
+import 'package:maral_cosmetics/models/brand.dart';
+import 'package:http/http.dart' as http;
+
+class BrandApiService {
+  // fetch brands -------------------------------------------------------
+  Future<ResultBrands> fetchBrands({
+    required int page,
+    required int pageSize,
+    required String search,
+  }) async {
+    Uri uri = Uri.parse('$apiUrl/news/all').replace(
+      queryParameters: {
+        'pageSize': pageSize.toString(),
+        'page': '$page',
+        'search': search,
+      },
+    );
+
+    try {
+      Response response = await http.get(uri);
+      var jsonData = json.decode(response.body);
+
+      if (response.statusCode == 200 && jsonData['success']) {
+        int pageCount = jsonData['data']['pageCount'] as int;
+
+        if (jsonData['data']['rows'] == []) {
+          return ResultBrands(
+            brands: const [],
+            pageCount: pageCount,
+            error: '',
+          );
+        }
+
+        var data = jsonData['data']['rows'] as List;
+        return ResultBrands(
+          brands:
+              data.map<Brand>((propJson) => Brand.fromJson(propJson)).toList(),
+          pageCount: pageCount,
+          error: '',
+        );
+      }
+      return const ResultBrands(brands: [], pageCount: 0, error: '');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
