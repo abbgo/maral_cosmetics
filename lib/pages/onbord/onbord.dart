18cab08fbea94497dfaeb3e98640a54d5a415123
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-16 07:00:47 +0500

    fix: update OnbordPage

--- lib/pages/onbord/onbord.dart
+++ lib/pages/onbord/onbord.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/onbord/parts/onbord_indicator.dart';
 import 'package:maral_cosmetics/pages/onbord/parts/onbord_one.dart';
 import 'package:maral_cosmetics/pages/onbord/parts/onbord_two.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
 
 class OnbordPage extends StatefulWidget {
   const OnbordPage({super.key});
@@ -21,11 +23,43 @@ class _OnbordPageState extends State<OnbordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: pageController,
+      body: Column(
         children: [
-          OnbordOne(pageController: pageController),
-          const OnbordTwo(),
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              children: [
+                OnbordOne(pageController: pageController),
+                const OnbordTwo(),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OnbordIndicator(color: defaultColor),
+                  const SizedBox(width: 5),
+                  OnbordIndicator(color: defaultLightColor),
+                ],
+              ),
+              const SizedBox(height: 53),
+              GestureDetector(
+                onTap: () => pageController.animateToPage(
+                  1,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                ),
+                child: CircleAvatar(
+                  radius: 34,
+                  backgroundColor: defaultColor,
+                  child:
+                      Icon(Icons.adaptive.arrow_forward, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/pages/onbord/parts/onbord_one.dart
+++ lib/pages/onbord/parts/onbord_one.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/functions/screen.dart';
-import 'package:maral_cosmetics/pages/onbord/parts/onbord_indicator.dart';
-import 'package:maral_cosmetics/styles/colors.dart';
 
 class OnbordOne extends StatelessWidget {
   const OnbordOne({super.key, required this.pageController});
@@ -11,6 +9,7 @@ class OnbordOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Image.asset(
@@ -18,43 +17,21 @@ class OnbordOne extends StatelessWidget {
           width: screenProperties(context).width,
           fit: BoxFit.fitWidth,
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 17),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 17),
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Text(
+              Text(
                 'collection of treatments',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 22),
               ),
-              const SizedBox(height: 10),
-              const Text(
+              SizedBox(height: 10),
+              Text(
                 'The Ordinary is born to disallow commodity to be disguised as ingenuity. The Ordinary is "Clinical formulations with integrity".',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OnbordIndicator(color: defaultColor),
-                  const SizedBox(width: 5),
-                  OnbordIndicator(color: defaultLightColor),
-                ],
-              ),
-              const SizedBox(height: 53),
-              GestureDetector(
-                onTap: () => pageController.animateToPage(
-                  1,
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeIn,
-                ),
-                child: CircleAvatar(
-                  radius: 34,
-                  backgroundColor: defaultColor,
-                  child:
-                      Icon(Icons.adaptive.arrow_forward, color: Colors.white),
-                ),
               ),
             ],
           ),
