fe674b8596cafd62d74c7e26d01fb16017874a12
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-16 17:04:32 +0500

    fix: update HomeAboutCosmetics and HomeAboutCosmeticsCard part widgets

--- lib/pages/parts/home_about_cosmetics/home_about_cosmetics.dart
+++ lib/pages/parts/home_about_cosmetics/home_about_cosmetics.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:maral_cosmetics/pages/parts/home_about_cosmetics/parts/home_about_cosmetics_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
+import 'package:maral_cosmetics/helpers/static_data.dart';
+import 'package:maral_cosmetics/models/about_cosmetic.dart';
+import 'package:maral_cosmetics/models/default_param.dart';
+import 'package:maral_cosmetics/pages/parts/home_about_cosmetics/parts/home_about_cosmetics_card.dart';
+import 'package:maral_cosmetics/providers/api/about_cosmetic.dart';
+import 'package:maral_cosmetics/providers/pages/about_cosmetics.dart';
 
-class HomeAboutCosmetics extends StatelessWidget {
+class HomeAboutCosmetics extends ConsumerWidget {
   const HomeAboutCosmetics({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool loading = ref.watch(loadAboutCosmeticsProvider);
+
+    return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        ListTile(
+        const ListTile(
           title: Text('Kosmetika barada', style: TextStyle(fontSize: 20)),
         ),
-        HomeAboutCosmeticsList(),
+        SizedBox(
+          height: 120,
+          child: Stack(
+            children: [
+              // HomeAboutCosmeticsList(),
+              ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final page = index ~/ pageSize + 1;
+                  final indexInPage = index % pageSize;
+
+                  DefaultParams arg =
+                      DefaultParams(page: page, pageSize: pageSize);
+
+                  final AsyncValue<ResultAboutCosmetic> resultAboutCosmetic =
+                      ref.watch(fetchAboutCosmeticsProvider(arg));
+
+                  return resultAboutCosmetic.when(
+                    data: (response) {
+                      if (response.error != '') {
+                        return null;
+                      }
+                      if (indexInPage >= response.aboutCosmetics!.length) {
+                        return null;
+                      }
+
+                      bool isLast = page == response.pageCount &&
+                          (indexInPage + 1) == response.aboutCosmetics!.length;
+
+                      AboutCosmetic aboutCosmetic =
+                          response.aboutCosmetics![indexInPage];
+                      // return Text(aboutCosmetic.id);
+
+                      return HomeAboutCosmeticsCard(
+                        aboutCosmetic: aboutCosmetic,
+                        isFirst: index == 0,
+                        isLast: isLast,
+                      );
+                    },
+                    error: (error, stackTrace) => errorMethod(error),
+                    loading: () {
+                      if (!loading) {
+                        Future.delayed(
+                          const Duration(),
+                          () => ref
+                              .read(loadAboutCosmeticsProvider.notifier)
+                              .state = true,
+                        );
+                      }
+                      return null;
+                    },
+                  );
+                },
+              ),
+              loading ? loadWidget : const SizedBox(),
+            ],
+          ),
+        ),
       ],
     );
   }
--- lib/pages/parts/home_about_cosmetics/parts/home_about_cosmetics_card.dart
+++ lib/pages/parts/home_about_cosmetics/parts/home_about_cosmetics_card.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
+import 'package:maral_cosmetics/helpers/static_data.dart';
+import 'package:maral_cosmetics/models/about_cosmetic.dart';
 
 class HomeAboutCosmeticsCard extends StatelessWidget {
-  const HomeAboutCosmeticsCard(
-      {super.key,
-      required this.image,
-      required this.rightMargin,
-      required this.leftMargin});
+  const HomeAboutCosmeticsCard({
+    super.key,
+    required this.aboutCosmetic,
+    required this.isFirst,
+    required this.isLast,
+  });
 
-  final String image;
-  final double rightMargin, leftMargin;
+  final AboutCosmetic aboutCosmetic;
+  final bool isLast, isFirst;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xffFDFDF9),
       padding: const EdgeInsets.all(6),
-      margin: EdgeInsets.only(left: leftMargin, right: rightMargin),
+      margin: EdgeInsets.only(left: isFirst ? 10 : 5, right: isLast ? 10 : 5),
       width: 310,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(2),
-            child: Image.asset(
-              image,
+            child: SizedBox(
               width: 105,
               height: 105,
-              fit: BoxFit.cover,
+              child: BlurHash(
+                errorBuilder: (context, error, stackTrace) => loadWidget,
+                curve: Curves.easeOut,
+                hash: aboutCosmetic.image.hashblur,
+                image: '$pathUrl/${aboutCosmetic.image.url}',
+                imageFit: BoxFit.cover,
+              ),
             ),
           ),
           const SizedBox(width: 5),
