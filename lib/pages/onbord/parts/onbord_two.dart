950270993da074db2ff071b98685d86d0df77539
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-16 06:45:18 +0500

    fix: update OnbordTwo part widget

--- lib/pages/onbord/parts/onbord_two.dart
+++ lib/pages/onbord/parts/onbord_two.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/functions/screen.dart';
+import 'package:maral_cosmetics/pages/onbord/parts/onbord_indicator.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
 
 class OnbordTwo extends StatelessWidget {
   const OnbordTwo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('OnbordTwo'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          "assets/images/onbord_2.png",
+          width: screenProperties(context).width,
+          fit: BoxFit.fitWidth,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 17),
+          child: Column(
+            children: [
+              const Text(
+                'collection of treatments',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'The Ordinary is born to disallow commodity to be disguised as ingenuity. The Ordinary is "Clinical formulations with integrity".',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OnbordIndicator(color: defaultColor),
+                  const SizedBox(width: 5),
+                  OnbordIndicator(color: defaultLightColor),
+                ],
+              ),
+              const SizedBox(height: 53),
+              CircleAvatar(
+                radius: 34,
+                backgroundColor: defaultColor,
+                child: Icon(Icons.adaptive.arrow_forward, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
