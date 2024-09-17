14ef0cb88c403a1a54cf7ad506cf7bdbfe0b2440
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-17 15:21:40 +0500

    feat: add showIntConnErr method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/snackbars.dart';
+import 'package:maral_cosmetics/notifiers/internet.dart';
 import 'package:maral_cosmetics/pages/bottom_navigation/bottom_navigation.dart';
 import 'package:maral_cosmetics/pages/onbord/onbord.dart';
+import 'package:maral_cosmetics/providers/internet.dart';
 import 'package:maral_cosmetics/providers/local_storadge.dart';
 
 class HomePage extends ConsumerWidget {
@@ -10,6 +13,15 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     bool isFirstTime = ref.watch(isFirstTimeProvider);
+
+    ref.listen(
+      connectivityStatusProviders,
+      (previous, next) {
+        if (next == ConnectivityStatus.isDisonnected) {
+          showIntConnErr(context);
+        }
+      },
+    );
 
     return isFirstTime ? const OnbordPage() : const BottomNavigationPage();
   }
