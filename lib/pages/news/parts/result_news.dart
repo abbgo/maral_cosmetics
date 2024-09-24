77bf20ef790ef3775089b12e7adc35591ca2a14e
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-24 17:49:45 +0500

    fix: update ResultNewsPart part widget

--- lib/pages/news/parts/result_news.dart
+++ lib/pages/news/parts/result_news.dart
@@ -4,7 +4,6 @@ import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
 import 'package:maral_cosmetics/helpers/static_data.dart';
 import 'package:maral_cosmetics/models/default_param.dart';
 import 'package:maral_cosmetics/models/news.dart';
-import 'package:maral_cosmetics/pages/parts/home_about_cosmetics/home_about_cosmetics.dart';
 import 'package:maral_cosmetics/pages/parts/home_news/parts/home_news_card.dart';
 import 'package:maral_cosmetics/pages/parts/no_result.dart';
 import 'package:maral_cosmetics/providers/api/news.dart';
@@ -19,62 +18,54 @@ class ResultNewsPart extends ConsumerWidget {
     final bool loading = ref.watch(loadNewsProvider);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.only(top: 20),
       child: Stack(
         children: [
           !hasNews
               ? const NoResult()
-              : ListView.builder(
-                  itemBuilder: (context, index) {
-                    final page = index ~/ pageSize + 1;
-                    final indexInPage = index % pageSize;
+              : Container(
+                  padding: const EdgeInsets.only(top: 10),
+                  color: const Color(0xffF5F5F5),
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      final page = index ~/ pageSize + 1;
+                      final indexInPage = index % pageSize;
 
-                    DefaultParams arg =
-                        DefaultParams(page: page, pageSize: pageSize);
+                      DefaultParams arg =
+                          DefaultParams(page: page, pageSize: pageSize);
 
-                    final AsyncValue<ResultNews> resultNews =
-                        ref.watch(fetchNewsProvider(arg));
+                      final AsyncValue<ResultNews> resultNews =
+                          ref.watch(fetchNewsProvider(arg));
 
-                    return resultNews.when(
-                      skipLoadingOnRefresh: true,
-                      skipLoadingOnReload: true,
-                      skipError: true,
-                      data: (response) {
-                        if (response.error != '') {
+                      return resultNews.when(
+                        skipLoadingOnRefresh: true,
+                        skipLoadingOnReload: true,
+                        skipError: true,
+                        data: (response) {
+                          if (response.error != '') {
+                            return null;
+                          }
+                          if (indexInPage >= response.newss!.length) {
+                            return null;
+                          }
+
+                          NewsModel news = response.newss![indexInPage];
+                          return HomeNewsCard(news: news);
+                        },
+                        error: (error, stackTrace) => errorMethod(error),
+                        loading: () {
+                          if (!loading) {
+                            Future.delayed(
+                              const Duration(),
+                              () => ref.read(loadNewsProvider.notifier).state =
+                                  true,
+                            );
+                          }
                           return null;
-                        }
-                        if (indexInPage >= response.newss!.length) {
-                          return null;
-                        }
-
-                        bool isLast = page == response.pageCount &&
-                            (indexInPage + 1) == response.newss!.length;
-
-                        NewsModel news = response.newss![indexInPage];
-                        return isLast
-                            ? Column(
-                                children: [
-                                  HomeNewsCard(news: news),
-                                  const SizedBox(height: 20),
-                                  const HomeAboutCosmetics(),
-                                  const SizedBox(height: 20),
-                                ],
-                              )
-                            : HomeNewsCard(news: news);
-                      },
-                      error: (error, stackTrace) => errorMethod(error),
-                      loading: () {
-                        if (!loading) {
-                          Future.delayed(
-                            const Duration(),
-                            () => ref.read(loadNewsProvider.notifier).state =
-                                true,
-                          );
-                        }
-                        return null;
-                      },
-                    );
-                  },
+                        },
+                      );
+                    },
+                  ),
                 ),
           loading ? loadWidget : const SizedBox(),
         ],
