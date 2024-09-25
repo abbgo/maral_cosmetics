4df33a88c98c027131a828082a22e1bed6afcd27
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-25 15:19:15 +0500

    fix: update ResultGalleryPart part widget

--- lib/pages/gallery/parts/result_gallery_part.dart
+++ lib/pages/gallery/parts/result_gallery_part.dart
@@ -21,72 +21,62 @@ class ResultGalleryPart extends ConsumerWidget {
       children: [
         !hasGalleries
             ? const NoResult()
-            : GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 10,
-                ),
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final page = index ~/ pageSize + 1;
-                  final indexInPage = index % pageSize;
+            : Padding(
+                padding: const EdgeInsets.only(left: 18, top: 18, right: 18),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    mainAxisExtent: 158,
+                  ),
+                  itemBuilder: (context, index) {
+                    final page = index ~/ pageSize + 1;
+                    final indexInPage = index % pageSize;
 
-                  DefaultParams arg =
-                      DefaultParams(page: page, pageSize: pageSize);
+                    DefaultParams arg =
+                        DefaultParams(page: page, pageSize: pageSize);
 
-                  final AsyncValue<ResultGallery> resultGallery =
-                      ref.watch(fetchGalleriesProvider(arg));
+                    final AsyncValue<ResultGallery> resultGallery =
+                        ref.watch(fetchGalleriesProvider(arg));
 
-                  return resultGallery.when(
-                    data: (response) {
-                      if (response.error != '') {
-                        return null;
-                      }
-                      if (indexInPage >= response.galleries!.length) {
-                        return null;
-                      }
+                    return resultGallery.when(
+                      data: (response) {
+                        if (response.error != '') {
+                          return null;
+                        }
+                        if (indexInPage >= response.galleries!.length) {
+                          return null;
+                        }
 
-                      bool isLast = page == response.pageCount &&
-                          (indexInPage + 1) == response.galleries!.length;
+                        Gallery gallery = response.galleries![indexInPage];
 
-                      Gallery gallery = response.galleries![indexInPage];
-
-                      return Container(
-                        margin: EdgeInsets.only(
-                          left: index == 0 ? 10 : 5,
-                          right: isLast ? 10 : 5,
-                        ),
-                        child: ClipRRect(
+                        return ClipRRect(
                           borderRadius: BorderRadius.circular(4),
-                          child: SizedBox(
-                            width: 140,
-                            height: 92,
-                            child: BlurHash(
-                              errorBuilder: (context, error, stackTrace) =>
-                                  loadWidget,
-                              curve: Curves.easeOut,
-                              hash: gallery.image.hashblur,
-                              image: '$pathUrl/${gallery.image.url}',
-                              imageFit: BoxFit.cover,
-                            ),
+                          child: BlurHash(
+                            errorBuilder: (context, error, stackTrace) =>
+                                loadWidget,
+                            curve: Curves.easeOut,
+                            hash: gallery.image.hashblur,
+                            image: '$pathUrl/${gallery.image.url}',
+                            imageFit: BoxFit.cover,
                           ),
-                        ),
-                      );
-                    },
-                    error: (error, stackTrace) => errorMethod(error),
-                    loading: () {
-                      if (!loading) {
-                        Future.delayed(
-                          const Duration(),
-                          () => ref.read(loadGalleryProvider.notifier).state =
-                              true,
                         );
-                      }
-                      return null;
-                    },
-                  );
-                },
+                      },
+                      error: (error, stackTrace) => errorMethod(error),
+                      loading: () {
+                        if (!loading) {
+                          Future.delayed(
+                            const Duration(),
+                            () => ref.read(loadGalleryProvider.notifier).state =
+                                true,
+                          );
+                        }
+                        return null;
+                      },
+                    );
+                  },
+                ),
               ),
         loading ? loadWidget : const SizedBox(),
       ],
