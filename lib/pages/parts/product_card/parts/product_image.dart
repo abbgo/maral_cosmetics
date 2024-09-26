1a76ef46c5a281fca5ccd3beb0b2399171298a7c
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-26 16:06:29 +0500

    fix: update ProductImage part widget

--- lib/pages/parts/product_card/parts/product_image.dart
+++ lib/pages/parts/product_card/parts/product_image.dart
@@ -25,6 +25,25 @@ class ProductImage extends StatelessWidget {
             ],
           ),
         ),
+        Positioned(
+          right: 5,
+          top: 5,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Icon(
+                Icons.favorite_outline,
+                color: Color(0xff824D6A),
+                size: 22,
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
