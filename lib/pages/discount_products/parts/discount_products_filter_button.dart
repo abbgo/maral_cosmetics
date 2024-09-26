1cef12a00f6385d19b63310de17ce40de8ce68ef
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-26 17:29:23 +0500

    feat: add DiscountProductsFilterButton part widget

---
+++ lib/pages/discount_products/parts/discount_products_filter_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DiscountProductsFilterButton extends StatelessWidget {
+  const DiscountProductsFilterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: const Color(0xffE0E0E0)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/filter.png'),
+            const Text('Filtrele'),
+          ],
+        ),
+      ),
+    );
+  }
+}
