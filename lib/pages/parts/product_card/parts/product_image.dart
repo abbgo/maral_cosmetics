cf56d24ca22141e035ac7f85ea7cf3afd95b18e3
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-26 15:55:59 +0500

    fix: update ProductImage part widget

--- lib/pages/parts/product_card/parts/product_image.dart
+++ lib/pages/parts/product_card/parts/product_image.dart
@@ -7,9 +7,52 @@ class ProductImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: Image.asset(image, fit: BoxFit.cover, height: 140),
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(image, fit: BoxFit.cover, height: 140),
+        ),
+        Positioned(
+          top: 5,
+          left: 5,
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  '54%',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                    fontFamily: 'HeyWowRegular',
+                  ),
+                ),
+              ),
+              const SizedBox(width: 2),
+              Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  'Taze',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                    fontFamily: 'HeyWowRegular',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
