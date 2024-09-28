1544288910e335a64e6d0161994483e9b0f7f518
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-28 17:33:21 +0500

    fix: update ProductPriceAndOrderButton part widget

--- lib/pages/parts/product_card/parts/product_order_button.dart
+++ lib/pages/parts/product_card/parts/product_order_button.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ProductOrderButton extends StatelessWidget {
-  const ProductOrderButton({super.key});
+  const ProductOrderButton({super.key, this.buttonWidth, this.buttonHeight});
+
+  final double? buttonWidth, buttonHeight;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 30,
-      width: double.maxFinite,
+      height: buttonHeight ?? 30,
+      width: buttonWidth ?? double.maxFinite,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xffA16F8A),
--- lib/pages/product/parts/product_price_and_order_button.dart
+++ lib/pages/product/parts/product_price_and_order_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/methods/parts/product_image.dart';
+import 'package:maral_cosmetics/pages/parts/product_card/parts/product_order_button.dart';
 import 'package:maral_cosmetics/pages/parts/product_card/parts/product_prices.dart';
 
 class ProductPriceAndOrderButton extends StatelessWidget {
@@ -14,10 +15,16 @@ class ProductPriceAndOrderButton extends StatelessWidget {
         color: const Color(0xffF8F8F8),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const ProductPrices(fontSizePrice: 20, fontSizeOldPrice: 16),
-          const SizedBox(width: 10),
-          imageIconMethod('54%', Colors.red, 14),
+          Row(
+            children: [
+              const ProductPrices(fontSizePrice: 20, fontSizeOldPrice: 16),
+              const SizedBox(width: 10),
+              imageIconMethod('54%', Colors.red, 14),
+            ],
+          ),
+          const ProductOrderButton(buttonWidth: 105, buttonHeight: 40),
         ],
       ),
     );
