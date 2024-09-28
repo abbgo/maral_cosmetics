f7e62c0e73f9955d2c530b3b23785d875f73a231
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-28 13:20:58 +0500

    fix: update HomeNewProductsResult part widget

--- lib/pages/parts/home_new_products/parts/home_new_products_result.dart
+++ lib/pages/parts/home_new_products/parts/home_new_products_result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/examples/static_variables.dart';
+import 'package:maral_cosmetics/pages/parts/product_card/product_card.dart';
 
 class HomeNewProductsResult extends StatelessWidget {
   const HomeNewProductsResult({super.key});
@@ -7,43 +8,18 @@ class HomeNewProductsResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 90,
+      height: 275,
       child: ListView.separated(
-        itemCount: homeBrendSliders.length,
         scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => Container(
-          margin: EdgeInsets.only(
-            left: index == 0 ? 10 : 0,
-            right: index == homeBrendSliders.length - 1 ? 10 : 0,
-          ),
-          child: Stack(
-            alignment: const Alignment(0, 0.8),
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  homeBrendSliders[index],
-                  width: 140,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                width: 105,
-                height: 20,
-                padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 6),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(.5),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: const Text(
-                  'Customers Says',
-                  style: TextStyle(fontSize: 10),
-                ),
-              ),
-            ],
-          ),
-        ),
+        itemBuilder: (context, index) {
+          return ProductCard(
+            image: homeDiscountProducts[index],
+            isFisrt: index == 0,
+            isLast: index == homeDiscountProducts.length - 1,
+          );
+        },
         separatorBuilder: (context, index) => const SizedBox(width: 10),
+        itemCount: homeDiscountProducts.length,
       ),
     );
   }
