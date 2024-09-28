f1a0ffad5af92a1778ce0107fe84ad26e9064887
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-28 13:27:36 +0500

    feat: add NewProductsGridview part widget and update NewProductsPage

--- lib/pages/new_products/new_products.dart
+++ lib/pages/new_products/new_products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/methods/parts/app_bar.dart';
+import 'package:maral_cosmetics/pages/new_products/parts/new_products_gridview.dart';
 
 class NewProductsPage extends StatelessWidget {
   const NewProductsPage({super.key});
@@ -8,6 +10,11 @@ class NewProductsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('In taze gosulanlar'),
+        bottom: appBarBottomMethod(),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: NewProductsGridview(),
       ),
     );
   }
