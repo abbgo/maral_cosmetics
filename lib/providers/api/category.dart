a34a7aed136497cf88c0d84e7aa1b44d219512b2
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-30 17:33:01 +0500

    feat: add fetchCategoriesProvider provider

--- lib/providers/api/category.dart
+++ lib/providers/api/category.dart
@@ -1,5 +1,37 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/models/category.dart';
+import 'package:maral_cosmetics/models/default_param.dart';
 import 'package:maral_cosmetics/services/api/category.dart';
 
 final categoryApiProvider =
     Provider<CategoryApiService>((ref) => CategoryApiService());
+
+var fetchCategoriesProvider =
+    FutureProvider.family<ResultCategory, DefaultParams>(
+  (ref, arg) async {
+    ResultCategory result = ResultCategory.defaultResult();
+
+    try {
+      String search = await ref.watch(brandSearchProvider);
+
+      ResultCategory resultBrands =
+          await ref.read(categoryApiProvider).fetchCategories(
+                page: arg.page!,
+                pageSize: arg.pageSize!,
+                search: search,
+              );
+
+      if (search != '') {
+        ref.read(hasBrandsProvider.notifier).state =
+            resultBrands.brands!.isNotEmpty;
+      }
+
+      result = resultBrands;
+    } catch (e) {
+      result = ResultCategory(error: e.toString());
+    }
+
+    ref.read(loadBrandProvider.notifier).state = false;
+    return result;
+  },
+);
