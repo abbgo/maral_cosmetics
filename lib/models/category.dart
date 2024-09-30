2173c0c8d56ea97dde61daa9253890788337ee9e
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-30 18:40:33 +0500

    fix: update CategoryCard part widget

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,16 +1,18 @@
 import 'package:equatable/equatable.dart';
 import 'package:maral_cosmetics/models/image.dart';
 
-class CategoryModel {
+class CategoryModel extends Equatable {
   final String id, name;
   final BlurImage image;
   final List<CategoryModel> subcategories;
+  final bool isExpanded;
 
   CategoryModel({
     required this.id,
     required this.name,
     required this.image,
     required this.subcategories,
+    required this.isExpanded,
   });
 
   factory CategoryModel.fromJson(Map<String, dynamic> json) {
@@ -25,8 +27,28 @@ class CategoryModel {
                 (categoryJson) => CategoryModel.fromJson(categoryJson),
               ),
             ),
+      isExpanded: false,
     );
   }
+
+  CategoryModel copyWith({
+    String? id,
+    String? name,
+    BlurImage? image,
+    List<CategoryModel>? subcategories,
+    bool? isExpanded,
+  }) {
+    return CategoryModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      image: image ?? this.image,
+      subcategories: subcategories ?? this.subcategories,
+      isExpanded: isExpanded ?? this.isExpanded,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, name, image, subcategories, isExpanded];
 }
 
 class ResultCategory extends Equatable {
--- lib/pages/parts/category_card/category_card.dart
+++ lib/pages/parts/category_card/category_card.dart
@@ -3,35 +3,46 @@ import 'package:maral_cosmetics/models/category.dart';
 import 'package:maral_cosmetics/pages/parts/category_card/parts/category_card_image.dart';
 import 'package:maral_cosmetics/pages/parts/category_card/parts/sub_category_card.dart';
 
-class CategoryCard extends StatelessWidget {
+class CategoryCard extends StatefulWidget {
   const CategoryCard({super.key, required this.category});
 
   final CategoryModel category;
 
   @override
+  State<CategoryCard> createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
+  @override
   Widget build(BuildContext context) {
+    print('------------------- ${widget.category.isExpanded}');
     return ExpansionTile(
       shape: const Border.symmetric(),
       tilePadding: const EdgeInsets.all(0),
       childrenPadding: const EdgeInsets.all(0),
       backgroundColor: const Color(0xfffffafc),
-      leading: CategoryCardImage(image: category.image),
+      leading: CategoryCardImage(image: widget.category.image),
       trailing: AnimatedRotation(
-        turns: _isExpanded ? 0.25 : 0.0,
+        turns: widget.category.isExpanded ? -0.25 : 0.0,
         duration: const Duration(milliseconds: 300),
         child: const Icon(Icons.arrow_drop_down),
       ),
       title: Text(
-        category.name,
+        widget.category.name,
         style: const TextStyle(
           fontFamily: 'Roboto',
           fontWeight: FontWeight.bold,
           fontSize: 16,
         ),
       ),
-      children: category.subcategories
+      children: widget.category.subcategories
           .map((e) => SubCategoryCard(subcategory: e))
           .toList(),
+      onExpansionChanged: (bool expanded) {
+        setState(() {
+          widget.category.copyWith(isExpanded: expanded);
+        });
+      },
     );
   }
 }
