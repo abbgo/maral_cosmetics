a86cd85a7856438137e53f28df509a00ede70e2e
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-30 16:01:21 +0500

    fix: update SimilarProducts part widget

--- lib/pages/product/parts/similar_products.dart
+++ lib/pages/product/parts/similar_products.dart
@@ -7,19 +7,22 @@ class SimilarProducts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text('Meňzeşler', style: TextStyle(fontSize: 20)),
-        const SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ProductCard(image: homeDiscountProducts.first),
-            ProductCard(image: homeDiscountProducts.first),
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Meňzeşler', style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ProductCard(image: homeDiscountProducts.first),
+              ProductCard(image: homeDiscountProducts.first),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
