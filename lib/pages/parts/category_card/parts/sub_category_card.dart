23ff9b46b940ba5558ee5f8cc71d4f66e1100a82
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-01 16:01:21 +0500

    fix: update SubCategoryCard part widget

--- lib/pages/parts/category_card/parts/sub_category_card.dart
+++ lib/pages/parts/category_card/parts/sub_category_card.dart
@@ -9,8 +9,22 @@ class SubCategoryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      tileColor: Colors.white,
-      title: Text(subcategory.name),
+      contentPadding: const EdgeInsets.all(0),
+      tileColor: const Color.fromRGBO(255, 255, 255, 1),
+      title: Text(
+        subcategory.name,
+        style: const TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      trailing: const RotatedBox(
+        quarterTurns: 3,
+        child: Icon(
+          Icons.arrow_drop_down,
+        ),
+      ),
     );
   }
 }
