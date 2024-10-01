aae4c872fb7ce1834d62e3096c1a2e7fb6f2d608
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-01 17:06:22 +0500

    feat: add showSortBottomSheet method

---
+++ lib/helpers/methods/parts/product_sort_button.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+showSortBottomSheet(BuildContext context) {
+  showBottomSheet(
+    context: context,
+    builder: (context) {},
+  );
+}
--- lib/pages/parts/product_sort_and_filter/parts/product_sort_button.dart
+++ lib/pages/parts/product_sort_and_filter/parts/product_sort_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/helpers/methods/parts/product_sort_button.dart';
 
 class ProductSortButton extends StatelessWidget {
   const ProductSortButton({super.key});
@@ -6,25 +7,28 @@ class ProductSortButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: const Color(0xffE0E0E0), width: 1.5),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/sort.png'),
-            const SizedBox(width: 10),
-            const Text(
-              'Tertiple',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'HeyWowRegular',
-              ),
-            )
-          ],
+      child: GestureDetector(
+        onTap: () => showSortBottomSheet(context),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: const Color(0xffE0E0E0), width: 1.5),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/sort.png'),
+              const SizedBox(width: 10),
+              const Text(
+                'Tertiple',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'HeyWowRegular',
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
