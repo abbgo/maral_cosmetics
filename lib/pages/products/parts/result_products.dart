0e2e8e89e6ed906e77c63a2059f266d9ee4c36a7
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-02 19:20:52 +0500

    fix: update ResultProducts part widget

--- lib/pages/products/parts/result_products.dart
+++ lib/pages/products/parts/result_products.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
 import 'package:maral_cosmetics/pages/parts/count_products.dart';
+import 'package:maral_cosmetics/pages/parts/no_result.dart';
 import 'package:maral_cosmetics/pages/parts/products_gridview.dart';
+import 'package:maral_cosmetics/providers/pages/products.dart';
 
-class ResultProducts extends StatelessWidget {
+class ResultProducts extends ConsumerWidget {
   const ResultProducts({super.key, this.categoryID, this.brandID});
 
   final String? categoryID, brandID;
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: ListView(
-        children: const [
-          CountProducts(),
-          ProductsGridview(),
-        ],
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool hasData = ref.watch(hasProductsProvider);
+    final bool loading = ref.watch(loadProductsProvider);
+
+    return Stack(
+      children: [
+        !hasData
+            ? const NoResult()
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ListView(
+                  children: const [
+                    CountProducts(),
+                    ProductsGridview(),
+                  ],
+                ),
+              ),
+        loading ? loadWidget : const SizedBox(),
+      ],
     );
   }
 }
