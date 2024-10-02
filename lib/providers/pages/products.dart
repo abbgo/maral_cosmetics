307705b0521b8c936d8e18302e0bd235acef23d8
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-02 19:15:28 +0500

    feat: add productSearchProvider , hasProductsProvider , loadProductsProvider providers

---
+++ lib/providers/pages/products.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+var productSearchProvider = StateProvider<String>((ref) => '');
+var hasProductsProvider = StateProvider.autoDispose<bool>((ref) => true);
+final loadProductsProvider = StateProvider<bool>((ref) => true);
