2e57e28bbd786a11b89c1b160ff5076974753e73
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-02 12:12:06 +0500

    feat: add PriceInput partw widget

---
+++ lib/pages/product_price_filter/parts/price_input.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PriceInput extends StatelessWidget {
+  const PriceInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: const TextField());
+  }
+}
--- lib/pages/product_price_filter/product_price_filter.dart
+++ lib/pages/product_price_filter/product_price_filter.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/methods/parts/app_bar.dart';
+import 'package:maral_cosmetics/pages/product_price_filter/parts/price_input.dart';
 
 class ProductPriceFilterPage extends StatelessWidget {
   const ProductPriceFilterPage({super.key});
@@ -18,10 +19,13 @@ class ProductPriceFilterPage extends StatelessWidget {
         ),
         bottom: appBarBottomMethod(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
         child: Row(
-          children: [],
+          children: [
+            PriceInput(),
+            PriceInput(),
+          ],
         ),
       ),
     );
