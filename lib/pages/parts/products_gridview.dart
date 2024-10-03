1f24272a5c4aada71c7bb01798dcd2180042b3d0
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-03 17:10:59 +0500

    fix: update ResultProducts part widget

--- lib/pages/parts/products_gridview.dart
+++ lib/pages/parts/products_gridview.dart
@@ -17,58 +17,49 @@ class ProductsGridview extends ConsumerWidget {
     bool hasData = ref.watch(hasProductsProvider);
     final bool loading = ref.watch(loadProductsProvider);
 
-    return Stack(
-      children: [
-        !hasData
-            ? const NoResult()
-            : GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                // itemCount: 8,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  mainAxisExtent: 275,
-                ),
-                itemBuilder: (context, index) {
-                  final page = index ~/ pageSize + 1;
-                  final indexInPage = index % pageSize;
+    return !hasData
+        ? const SliverFillRemaining(hasScrollBody: true, child: NoResult())
+        : SliverGrid.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 275,
+            ),
+            itemBuilder: (context, index) {
+              final page = index ~/ pageSize + 1;
+              final indexInPage = index % pageSize;
 
-                  DefaultParams arg =
-                      DefaultParams(page: page, pageSize: pageSize);
+              DefaultParams arg = DefaultParams(page: page, pageSize: pageSize);
 
-                  AsyncValue<ResultProduct> resultProducts =
-                      ref.watch(fetchProductsProvider(arg));
+              AsyncValue<ResultProduct> resultProducts =
+                  ref.watch(fetchProductsProvider(arg));
 
-                  return resultProducts.when(
-                    data: (response) {
-                      if (response.error != '') {
-                        return null;
-                      }
-                      if (indexInPage >= response.products!.length) {
-                        return null;
-                      }
+              return resultProducts.when(
+                data: (response) {
+                  if (response.error != '') {
+                    return null;
+                  }
+                  if (indexInPage >= response.products!.length) {
+                    return null;
+                  }
 
-                      Product product = response.products![indexInPage];
-                      return ProductCard(product: product);
-                    },
-                    error: (error, stackTrace) => errorMethod(error),
-                    loading: () {
-                      if (!loading) {
-                        Future.delayed(
-                          const Duration(),
-                          () => ref.read(loadProductsProvider.notifier).state =
-                              true,
-                        );
-                      }
-                      return null;
-                    },
-                  );
+                  Product product = response.products![indexInPage];
+                  return ProductCard(product: product);
                 },
-              ),
-        loading ? loadWidget : const SizedBox(),
-      ],
-    );
+                error: (error, stackTrace) => errorMethod(error),
+                loading: () {
+                  if (!loading) {
+                    Future.delayed(
+                      const Duration(),
+                      () =>
+                          ref.read(loadProductsProvider.notifier).state = true,
+                    );
+                  }
+                  return null;
+                },
+              );
+            },
+          );
   }
 }
--- lib/pages/products/parts/result_products.dart
+++ lib/pages/products/parts/result_products.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
 import 'package:maral_cosmetics/pages/parts/count_products.dart';
 import 'package:maral_cosmetics/pages/parts/products_gridview.dart';
+import 'package:maral_cosmetics/providers/pages/products.dart';
 
-class ResultProducts extends StatelessWidget {
+class ResultProducts extends ConsumerWidget {
   const ResultProducts({super.key, this.categoryID, this.brandID});
 
   final String? categoryID, brandID;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool loading = ref.watch(loadProductsProvider);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: ListView(
-        children: const [
-          CountProducts(),
-          ProductsGridview(),
+      child: Stack(
+        children: [
+          const CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                toolbarHeight: 70,
+                flexibleSpace: CountProducts(),
+              ),
+              ProductsGridview(),
+            ],
+          ),
+          loading ? loadWidget : const SizedBox.shrink(),
         ],
       ),
     );
