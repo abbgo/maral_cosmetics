90eb68f3d8705adafad7aca60c0da4fbc8dc59e3
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-03 17:23:46 +0500

    feat: add categoryIDsProvider provider

--- lib/providers/pages/products.dart
+++ lib/providers/pages/products.dart
@@ -3,3 +3,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 var productSearchProvider = StateProvider<String>((ref) => '');
 var hasProductsProvider = StateProvider.autoDispose<bool>((ref) => true);
 final loadProductsProvider = StateProvider<bool>((ref) => true);
+final categoryIDsProvider = StateProvider<List<String>>((ref) => []);
