74d1a89c2dc33306cc4f3f9efd2b5b756a35a546
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-03 11:42:22 +0500

    fix: update ResultProducts and ProductsGridview part widgets

--- lib/pages/parts/products_gridview.dart
+++ lib/pages/parts/products_gridview.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/examples/static_variables.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
+import 'package:maral_cosmetics/pages/parts/no_result.dart';
 import 'package:maral_cosmetics/pages/parts/product_card/product_card.dart';
+import 'package:maral_cosmetics/providers/pages/products.dart';
 
-class ProductsGridview extends StatelessWidget {
+class ProductsGridview extends ConsumerWidget {
   const ProductsGridview({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: 8,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        mainAxisExtent: 275,
-      ),
-      itemBuilder: (context, index) =>
-          ProductCard(image: homeDiscountProducts.first),
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool hasData = ref.watch(hasProductsProvider);
+    final bool loading = ref.watch(loadProductsProvider);
+
+    return Stack(
+      children: [
+        !hasData
+            ? const NoResult()
+            : GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 8,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  mainAxisExtent: 275,
+                ),
+                itemBuilder: (context, index) =>
+                    ProductCard(image: homeDiscountProducts.first),
+              ),
+        loading ? loadWidget : const SizedBox(),
+      ],
     );
   }
 }
--- lib/pages/products/parts/result_products.dart
+++ lib/pages/products/parts/result_products.dart
@@ -1,36 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
 import 'package:maral_cosmetics/pages/parts/count_products.dart';
-import 'package:maral_cosmetics/pages/parts/no_result.dart';
 import 'package:maral_cosmetics/pages/parts/products_gridview.dart';
-import 'package:maral_cosmetics/providers/pages/products.dart';
 
-class ResultProducts extends ConsumerWidget {
+class ResultProducts extends StatelessWidget {
   const ResultProducts({super.key, this.categoryID, this.brandID});
 
   final String? categoryID, brandID;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    bool hasData = ref.watch(hasProductsProvider);
-    final bool loading = ref.watch(loadProductsProvider);
-
-    return Stack(
-      children: [
-        !hasData
-            ? const NoResult()
-            : Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ListView(
-                  children: const [
-                    CountProducts(),
-                    ProductsGridview(),
-                  ],
-                ),
-              ),
-        loading ? loadWidget : const SizedBox(),
-      ],
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: ListView(
+        children: const [
+          CountProducts(),
+          ProductsGridview(),
+        ],
+      ),
     );
   }
 }
