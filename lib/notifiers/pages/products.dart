9fff242a28cb48db85da878651a44d4d587e6fee
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-04 17:14:31 +0500

    feat: add BrandIDsNotifier notifier

--- lib/notifiers/pages/products.dart
+++ lib/notifiers/pages/products.dart
@@ -3,11 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class CategoryIDsNotifier extends StateNotifier<List<String>> {
   CategoryIDsNotifier() : super([]);
 
-  Future<void> addCategoryID(String id) async {
+  Future<void> setCategoryID(String id) async {
+    state = [];
     state = [...state, id];
   }
+}
 
-  Future<void> setCategoryID(String id) async {
+class BrandIDsNotifier extends StateNotifier<List<String>> {
+  BrandIDsNotifier() : super([]);
+
+  Future<void> setBrandID(String id) async {
     state = [];
     state = [...state, id];
   }
