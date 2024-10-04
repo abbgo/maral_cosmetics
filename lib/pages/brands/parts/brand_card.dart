d14ad292542ce8922051026ef9c74341423864b8
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-04 17:18:46 +0500

    fix: update BrandCard part widget

--- lib/pages/brands/parts/brand_card.dart
+++ lib/pages/brands/parts/brand_card.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/helpers/methods/navigation.dart';
 import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
 import 'package:maral_cosmetics/helpers/static_data.dart';
 import 'package:maral_cosmetics/models/brand.dart';
 import 'package:maral_cosmetics/pages/products/products.dart';
+import 'package:maral_cosmetics/providers/pages/products.dart';
 
-class BrandCard extends StatelessWidget {
+class BrandCard extends ConsumerWidget {
   const BrandCard({super.key, required this.brand});
 
   final Brand brand;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
-      onTap: () => Navigator.push(
-        context,
-        CustomPageRoute(
-          child: ProductsPage(brand: brand),
-          direction: AxisDirection.left,
-        ),
-      ),
+      onTap: () async {
+        await ref.read(brandIDsProvider.notifier).setBrandID(brand.id);
+
+        if (context.mounted) {
+          Navigator.push(
+            context,
+            CustomPageRoute(
+              child: ProductsPage(brand: brand),
+              direction: AxisDirection.left,
+            ),
+          );
+        }
+      },
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
