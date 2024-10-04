d447da597588b59a6c107cdf9ca7cbbe4260ef65
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-04 17:38:46 +0500

    fix: update fetchProductsProvider provider

--- lib/providers/api/product.dart
+++ lib/providers/api/product.dart
@@ -3,6 +3,7 @@ import 'package:maral_cosmetics/models/default_param.dart';
 import 'package:maral_cosmetics/models/product.dart';
 import 'package:maral_cosmetics/providers/local_storadge.dart';
 import 'package:maral_cosmetics/providers/pages/products.dart';
+import 'package:maral_cosmetics/providers/parts/product_sort_and_filter.dart';
 import 'package:maral_cosmetics/services/api/product.dart';
 
 final productApiProvider =
@@ -17,11 +18,12 @@ var fetchProductsProvider = FutureProvider.family<ResultProduct, DefaultParams>(
       String search = await ref.watch(productSearchProvider);
       List<String> categoryIDs = await ref.watch(categoryIDsProvider);
       List<String> brandIDs = await ref.watch(brandIDsProvider);
+      String sortProduct = await ref.watch(sortProductProvider);
 
       ProductParams productParams = ProductParams(
         categories: categoryIDs,
         brands: brandIDs,
-        ordering: 'all',
+        ordering: sortProduct,
         search: search,
         page: arg.page!,
         pageSize: arg.pageSize!,
