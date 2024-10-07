54de6c022aac94e6c55329730e28cf56e28e6c27
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-07 18:33:39 +0500

    fix: update SearchHistoryListTile part widget

--- lib/pages/search_product/parts/search_history_list_tile.dart
+++ lib/pages/search_product/parts/search_history_list_tile.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/providers/database/search.dart';
+import 'package:maral_cosmetics/providers/pages/products.dart';
+import 'package:maral_cosmetics/providers/pages/search_product.dart';
 
-class SearchHistoryListTile extends StatelessWidget {
+class SearchHistoryListTile extends ConsumerWidget {
   const SearchHistoryListTile({super.key, required this.search});
 
   final String search;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
+      onTap: () {
+        ref.read(productSearchProvider.notifier).state = search;
+        ref.read(openSearchHistoryProvider.notifier).state = false;
+      },
       contentPadding: const EdgeInsets.only(right: 0),
       dense: true,
       leading: const Icon(
@@ -24,13 +30,11 @@ class SearchHistoryListTile extends StatelessWidget {
           color: Color(0xff666666),
         ),
       ),
-      trailing: Consumer(
-        builder: (context, ref, child) => IconButton(
-          onPressed: () => ref.read(removeSearchProvider(search)),
-          icon: const Icon(
-            Icons.close,
-            color: Color(0xffE0E0E0),
-          ),
+      trailing: IconButton(
+        onPressed: () => ref.read(removeSearchProvider(search)),
+        icon: const Icon(
+          Icons.close,
+          color: Color(0xffE0E0E0),
         ),
       ),
     );
