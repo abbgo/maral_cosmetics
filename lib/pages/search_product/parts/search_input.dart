26d585aaf9532b892bdfaa521a0822a765b26ec6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-07 18:43:52 +0500

    feat: add openClearButtonProvider and searchTextProvider providers and update SearchInput part widget

--- lib/pages/search_product/parts/search_input.dart
+++ lib/pages/search_product/parts/search_input.dart
@@ -4,89 +4,67 @@ import 'package:maral_cosmetics/providers/database/search.dart';
 import 'package:maral_cosmetics/providers/pages/products.dart';
 import 'package:maral_cosmetics/providers/pages/search_product.dart';
 
-class SearchInput extends StatefulWidget {
+class SearchInput extends ConsumerWidget {
   const SearchInput({super.key});
 
   @override
-  State<SearchInput> createState() => _SearchInputState();
-}
-
-class _SearchInputState extends State<SearchInput> {
-  bool openClear = false;
-  TextEditingController searchCtrl = TextEditingController();
-
-  @override
-  void dispose() {
-    searchCtrl.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const outlineInputBorder = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(5)),
       borderSide: BorderSide(color: Color(0xfff2ebee)),
     );
 
-    return Consumer(
-      builder: (context, ref, child) {
-        return SizedBox(
-          height: 40,
-          child: TextField(
-            controller: searchCtrl,
-            autofocus: true,
-            textInputAction: TextInputAction.search,
-            cursorColor: const Color(0xffCCCCCC),
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              fillColor: const Color(0xfff6f1f3),
-              enabledBorder: outlineInputBorder,
-              focusedBorder: outlineInputBorder,
-              hintText: 'SEARCH',
-              hintStyle: const TextStyle(fontSize: 14),
-              prefixIcon: const Icon(
-                Icons.search,
-                size: 22,
-                color: Color(0xffA16F8A),
-              ),
-              suffixIcon: openClear
-                  ? GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          searchCtrl.clear();
-                          openClear = false;
-                          ref.read(productSearchProvider.notifier).state = '';
-                          ref.read(openSearchHistoryProvider.notifier).state =
-                              true;
-                        });
-                      },
-                      child: const Icon(
-                        Icons.cancel,
-                        color: Color(0xffADADAD),
-                      ),
-                    )
-                  : null,
-            ),
-            onChanged: (value) {
-              setState(() {
-                if (value.isNotEmpty) {
-                  openClear = true;
-                  ref.read(openSearchHistoryProvider.notifier).state = true;
-                } else {
-                  openClear = false;
-                }
-              });
-            },
-            onSubmitted: (value) async {
-              if (value.isNotEmpty) {
-                await ref.read(createSearchProvider(value).future);
-                ref.read(productSearchProvider.notifier).state = value;
-                ref.read(openSearchHistoryProvider.notifier).state = false;
-              }
-            },
+    bool openClearButton = ref.watch(openClearButtonProvider);
+
+    return SizedBox(
+      height: 40,
+      child: TextField(
+        autofocus: true,
+        textInputAction: TextInputAction.search,
+        cursorColor: const Color(0xffCCCCCC),
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+          fillColor: const Color(0xfff6f1f3),
+          enabledBorder: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
+          hintText: 'SEARCH',
+          hintStyle: const TextStyle(fontSize: 14),
+          prefixIcon: const Icon(
+            Icons.search,
+            size: 22,
+            color: Color(0xffA16F8A),
           ),
-        );
-      },
+          suffixIcon: openClearButton
+              ? GestureDetector(
+                  onTap: () {
+                    ref.read(openClearButtonProvider.notifier).state = false;
+                    ref.read(productSearchProvider.notifier).state = '';
+                    ref.read(openSearchHistoryProvider.notifier).state = true;
+                  },
+                  child: const Icon(
+                    Icons.cancel,
+                    color: Color(0xffADADAD),
+                  ),
+                )
+              : null,
+        ),
+        onChanged: (value) {
+          if (value.isNotEmpty) {
+            ref.read(openClearButtonProvider.notifier).state = true;
+            ref.read(openSearchHistoryProvider.notifier).state = true;
+          } else {
+            ref.read(openClearButtonProvider.notifier).state = false;
+          }
+        },
+        onSubmitted: (value) async {
+          if (value.isNotEmpty) {
+            await ref.read(createSearchProvider(value).future);
+            ref.read(productSearchProvider.notifier).state = value;
+            ref.read(openSearchHistoryProvider.notifier).state = false;
+          }
+        },
+      ),
     );
+    ;
   }
 }
--- lib/providers/pages/search_product.dart
+++ lib/providers/pages/search_product.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var openSearchHistoryProvider = StateProvider.autoDispose<bool>((ref) => true);
+var openClearButtonProvider = StateProvider.autoDispose<bool>((ref) => false);
+var searchTextProvider = StateProvider.autoDispose<String>((ref) => '');
