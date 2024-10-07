ae752510603138ac039ae695b3740fdc4cb09fc3
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-07 14:48:23 +0500

    fix: update ProductPage

--- lib/pages/product/product.dart
+++ lib/pages/product/product.dart
@@ -26,9 +26,9 @@ class ProductPage extends StatelessWidget {
             const SizedBox(height: 12),
             const ProductCategoriesAndStar(),
             const SizedBox(height: 12),
-            const Text(
-              'FLORMAR Göz üçin galam Eyeliner Pencil (Ultra Black)',
-              style: TextStyle(
+            Text(
+              product.name,
+              style: const TextStyle(
                 fontFamily: 'Inter',
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
@@ -37,10 +37,9 @@ class ProductPage extends StatelessWidget {
             const SizedBox(height: 12),
             const ProductPriceAndOrderButton(),
             const SizedBox(height: 12),
-            const ProductDescription(
+            ProductDescription(
               title: 'Barada',
-              description:
-                  'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. ',
+              description: product.description,
             ),
             const SimilarProducts(),
           ],
