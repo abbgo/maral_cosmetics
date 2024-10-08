f738d56a0ca569f2d60df4f26a646aa87302c626
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-08 18:58:02 +0500

    fix: update ProductOrderButton part widget

--- lib/pages/parts/product_card/parts/product_order_button.dart
+++ lib/pages/parts/product_card/parts/product_order_button.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/navigation.dart';
+import 'package:maral_cosmetics/pages/login.dart';
+import 'package:maral_cosmetics/providers/local_storadge.dart';
 
-class ProductOrderButton extends StatelessWidget {
+class ProductOrderButton extends ConsumerWidget {
   const ProductOrderButton({super.key, this.buttonWidth, this.buttonHeight});
 
   final double? buttonWidth, buttonHeight;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    String refreshToken = ref.watch(refreshTokenProvider);
+
     return SizedBox(
       height: buttonHeight ?? 30,
       width: buttonWidth ?? double.maxFinite,
@@ -14,7 +20,18 @@ class ProductOrderButton extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xffA16F8A),
         ),
-        onPressed: () {},
+        onPressed: () {
+          if (refreshToken.isEmpty) {
+            Navigator.push(
+              context,
+              CustomPageRoute(
+                child: const LoginPage(),
+                direction: AxisDirection.left,
+              ),
+            );
+            return;
+          }
+        },
         child: const Icon(
           Icons.local_mall_outlined,
           size: 18,
