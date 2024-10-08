e40ebbfc8088e5558c2715961d28cafec3b1309a
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-08 18:08:58 +0500

    fix: update RegisterPage

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -7,8 +7,24 @@ import 'package:maral_cosmetics/pages/parts/register_button.dart';
 import 'package:maral_cosmetics/pages/parts/phone_input.dart';
 import 'package:maral_cosmetics/pages/parts/select_language_button.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormState> registerFormKey = GlobalKey<FormState>();
+  final TextEditingController phoneCtrl = TextEditingController();
+  final TextEditingController passwordCtrl = TextEditingController();
+
+  @override
+  void dispose() {
+    phoneCtrl.dispose();
+    passwordCtrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,29 +35,29 @@ class RegisterPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Image.asset("assets/images/logo.jpg"),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Form(
                 child: Column(
                   children: [
-                    Text(
+                    const Text(
                       'Hasaba durmak',
                       style: TextStyle(fontSize: 20, color: Color(0xff824D6A)),
                     ),
-                    SizedBox(height: 30),
-                    PhoneInput(),
-                    SizedBox(height: 20),
-                    FullnameInput(),
-                    SizedBox(height: 20),
-                    PasswordInput(),
-                    SizedBox(height: 20),
-                    ConfirmPasswordInput(),
-                    SizedBox(height: 80),
-                    RegisterButton(forLogin: false),
-                    SizedBox(height: 10),
-                    LoginButton(forLogin: false),
-                    SizedBox(height: 20),
-                    SelectLanguageButton(),
+                    const SizedBox(height: 30),
+                    PhoneInput(phoneCtrl: phoneCtrl),
+                    const SizedBox(height: 20),
+                    const FullnameInput(),
+                    const SizedBox(height: 20),
+                    PasswordInput(passwordCtrl: passwordCtrl),
+                    const SizedBox(height: 20),
+                    const ConfirmPasswordInput(),
+                    const SizedBox(height: 80),
+                    const RegisterButton(forLogin: false),
+                    const SizedBox(height: 10),
+                    const LoginButton(forLogin: false),
+                    const SizedBox(height: 20),
+                    const SelectLanguageButton(),
                   ],
                 ),
               ),
