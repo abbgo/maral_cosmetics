209cc902d241485483089e565014a6b23bc3f340
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-08 17:19:34 +0500

    fix: update PasswordInput part widget

--- lib/pages/parts/password_input.dart
+++ lib/pages/parts/password_input.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/providers/parts/password_input.dart';
 
-class PasswordInput extends StatelessWidget {
+class PasswordInput extends ConsumerWidget {
   const PasswordInput({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const outlineInputBorder = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(5)),
       borderSide: BorderSide(color: Color(0xff79747E)),
     );
 
+    bool showPassword = ref.watch(showPasswordProvider);
+
     return TextFormField(
       cursorColor: Colors.black,
       style: const TextStyle(fontFamily: 'Inter'),
-      obscureText: true,
+      obscureText: !showPassword,
       obscuringCharacter: '*',
       decoration: const InputDecoration(
         enabledBorder: outlineInputBorder,
