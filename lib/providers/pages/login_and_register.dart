b2663e57f12e9eb318e7bcb29f4cb60cc98894cc
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 16:16:39 +0500

    feat: add loadOTPProvider provider

--- lib/providers/pages/login_and_register.dart
+++ lib/providers/pages/login_and_register.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var buttonPressProvider = StateProvider.autoDispose<bool>((ref) => false);
+var loadOTPProvider = StateProvider.autoDispose<bool>((ref) => false);
