e3c5fbf542816f6fef0a4409ad302852798d39a2
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:34:45 +0500

    fix: update LanguageSetting part widget

--- lib/pages/setting/parts/language_setting.dart
+++ lib/pages/setting/parts/language_setting.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/methods/bottom_sheets.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class LanguageSetting extends StatelessWidget {
   const LanguageSetting({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        onTap: () => showLanguageBottomSheet(context),
-        leading: Icon(Icons.language, color: defaultColor),
-        title: Text(
-          'Dil saylan',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Turkmen dili',
-              style: TextStyle(
-                color: defaultColor,
-                fontFamily: 'HeyWowRegular',
-                fontSize: 16,
-              ),
+    return ListTile(
+      shape: bottomBorder,
+      onTap: () => showLanguageBottomSheet(context),
+      leading: Icon(Icons.language, color: defaultColor),
+      title: Text(
+        'Dil saylan',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Turkmen dili',
+            style: TextStyle(
+              color: defaultColor,
+              fontFamily: 'HeyWowRegular',
+              fontSize: 16,
             ),
-            const SizedBox(width: 5),
-            Icon(Icons.adaptive.arrow_forward, color: defaultColor, size: 20),
-          ],
-        ),
+          ),
+          const SizedBox(width: 5),
+          Icon(Icons.adaptive.arrow_forward, color: defaultColor, size: 20),
+        ],
       ),
     );
   }
