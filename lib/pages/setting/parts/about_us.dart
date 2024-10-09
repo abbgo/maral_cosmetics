4e2635be9a8e8ca4f69469aa994a7e83191e1ae4
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:35:50 +0500

    fix: update AboutUs part widget

--- lib/pages/setting/parts/about_us.dart
+++ lib/pages/setting/parts/about_us.dart
@@ -2,27 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/methods/navigation.dart';
 import 'package:maral_cosmetics/pages/about_us.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class AboutUs extends StatelessWidget {
   const AboutUs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        onTap: () => Navigator.push(
-          context,
-          CustomPageRoute(
-            child: const AboutUsPage(),
-            direction: AxisDirection.left,
-          ),
+    return ListTile(
+      shape: bottomBorder,
+      onTap: () => Navigator.push(
+        context,
+        CustomPageRoute(
+          child: const AboutUsPage(),
+          direction: AxisDirection.left,
         ),
-        leading: Icon(Icons.person_pin_circle_outlined, color: defaultColor),
-        title: Text(
-          'Biz barada',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
+      ),
+      leading: Icon(Icons.person_pin_circle_outlined, color: defaultColor),
+      title: Text(
+        'Biz barada',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
       ),
     );
   }
