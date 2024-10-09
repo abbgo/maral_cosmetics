bcd84fa38dac317bbeead7dc642649ce4f4ce5c6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:35:20 +0500

    fix: update MyOrders part widget

--- lib/pages/setting/parts/my_orders.dart
+++ lib/pages/setting/parts/my_orders.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class MyOrders extends StatelessWidget {
   const MyOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        leading: Icon(Icons.local_mall_outlined, color: defaultColor),
-        title: Text(
-          'Menin sargytlarym',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
+    return ListTile(
+      shape: bottomBorder,
+      leading: Icon(Icons.local_mall_outlined, color: defaultColor),
+      title: Text(
+        'Menin sargytlarym',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
       ),
     );
   }
