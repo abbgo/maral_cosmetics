446989d2ac44db8a43269deb1c0d91fea0fd7660
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:33:49 +0500

    fix: update NotificationsSetting part widget

--- lib/pages/setting/parts/notifications_setting.dart
+++ lib/pages/setting/parts/notifications_setting.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class NotificationsSetting extends StatelessWidget {
   const NotificationsSetting({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        leading: Icon(Icons.notifications_outlined, color: defaultColor),
-        title: Text(
-          'Bildirisler',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
-        trailing: Switch(
-          activeColor: defaultColor,
-          value: true,
-          onChanged: (value) {},
-        ),
+    return ListTile(
+      shape: bottomBorder,
+      leading: Icon(Icons.notifications_outlined, color: defaultColor),
+      title: Text(
+        'Bildirisler',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
+      ),
+      trailing: Switch(
+        activeColor: defaultColor,
+        value: true,
+        onChanged: (value) {},
       ),
     );
   }
