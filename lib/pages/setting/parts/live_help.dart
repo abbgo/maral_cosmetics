d97a0da38e86ae792ff72e995f6a107e7e8e1786
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:37:07 +0500

    fix: update LiveHelp part widget

--- lib/pages/setting/parts/live_help.dart
+++ lib/pages/setting/parts/live_help.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class LiveHelp extends StatelessWidget {
   const LiveHelp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        leading: Icon(Icons.live_help_outlined, color: defaultColor),
-        title: Text(
-          'Salgylanma',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
+    return ListTile(
+      shape: bottomBorder,
+      leading: Icon(Icons.live_help_outlined, color: defaultColor),
+      title: Text(
+        'Salgylanma',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
       ),
     );
   }
