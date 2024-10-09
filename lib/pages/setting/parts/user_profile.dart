24e1d5ae10e8f88f201ab3b51c94b4e8651aa404
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:57:42 +0500

    fix: update UserProfile part widget

--- lib/pages/setting/parts/user_profile.dart
+++ lib/pages/setting/parts/user_profile.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
+import 'package:maral_cosmetics/models/user.dart';
+import 'package:maral_cosmetics/providers/database/user.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
 import 'package:maral_cosmetics/styles/style_borders.dart';
 
-class UserProfile extends StatelessWidget {
+class UserProfile extends ConsumerWidget {
   const UserProfile({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      shape: bottomBorder,
-      leading: Icon(Icons.account_circle, color: defaultColor, size: 50),
-      title: Text(
-        'Ahmet',
-        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-      ),
-      subtitle: Text(
-        '+993 63 509004',
-        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<User?> user = ref.watch(getUserProvider);
+
+    return user.when(
+      data: (data) {
+        if (data == null) {
+          return const SizedBox.shrink();
+        }
+
+        return ListTile(
+          shape: bottomBorder,
+          leading: Icon(Icons.account_circle, color: defaultColor, size: 50),
+          title: Text(
+            data.name!,
+            style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
+          ),
+          subtitle: Text(
+            data.phone,
+            style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
+          ),
+        );
+      },
+      error: (error, stackTrace) => errorMethod(error),
+      loading: () => loadWidget,
     );
   }
 }
