cc2680caeae696b10e9cab78bc7228c432f19ea5
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 13:47:34 +0500

    feat: add registerUser function

--- lib/services/api/user.dart
+++ lib/services/api/user.dart
@@ -1,3 +1,28 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class UserApiService {}
+import 'package:http/http.dart' as http;
+import 'package:maral_cosmetics/helpers/static_data.dart';
+import 'package:maral_cosmetics/models/user.dart';
+
+class UserApiService {
+  // user register ------------------------------------------------------------
+  Future<bool> registerUser(User user) async {
+    Uri uri = Uri.parse('$apiUrl/users/auth/sign-up');
+
+    try {
+      http.Response response = await http.post(
+        uri,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(user.toJson()),
+      );
+
+      dynamic jsonData = json.decode(response.body);
+      if (response.statusCode == 200 && jsonData['success']) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
