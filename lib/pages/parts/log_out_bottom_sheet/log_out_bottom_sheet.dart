2e8c7bb81fb296d6d4aa859f8e2f85e91f693b27
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 19:07:51 +0500

    feat: add bottomNavigationEdgeInsets variable and update LogOutBottomSheet part widget

--- lib/pages/parts/log_out_bottom_sheet/log_out_bottom_sheet.dart
+++ lib/pages/parts/log_out_bottom_sheet/log_out_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/pages/parts/top_widget_of_bottom_navigation.dart';
+import 'package:maral_cosmetics/styles/margin_styles.dart';
 
 class LogOutBottomSheet extends StatelessWidget {
   const LogOutBottomSheet({super.key});
@@ -6,63 +8,58 @@ class LogOutBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // padding: const EdgeInsets.symmetric(horizontal: 40),
-      // decoration: const BoxDecoration(
-      //   color: Color(0xffF8F2F5),
-      //   borderRadius: BorderRadius.only(
-      //     topLeft: Radius.circular(10),
-      //     topRight: Radius.circular(10),
-      //   ),
-      // ),
-      margin: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+      margin: bottomNavigationEdgeInsets,
       child: Wrap(
         alignment: WrapAlignment.center,
         children: [
+          const TopWidgetOfBottomNavigation(),
           Container(
-            margin: const EdgeInsets.only(top: 10),
-            width: 40,
-            height: 4,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.only(top: 8),
             decoration: BoxDecoration(
-              color: const Color(0xff824D6A),
-              borderRadius: BorderRadius.circular(60),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24),
             ),
-          ),
-          const SizedBox(height: 40),
-          const Text(
-            'Siz hakykyatdanam cykmak isleyanizmi?',
-            style: TextStyle(
-                color: Color(0xff824D6A), fontFamily: 'HeyWowRegular'),
-          ),
-          const SizedBox(height: 40),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xffc2a1b3),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 55),
+            child: Column(
+              children: [
+                const Text(
+                  'Siz hakykyatdanam cykmak isleyanizmi?',
+                  style: TextStyle(
+                      color: Color(0xff824D6A), fontFamily: 'HeyWowRegular'),
                 ),
-                onPressed: () => Navigator.pop(context),
-                child: const Text(
-                  'Yok',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffc2a1b3),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 55),
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text(
+                        'Yok',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff824D6A),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 55),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Hawa',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xff824D6A),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 55),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Hawa',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/styles/margin_styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+EdgeInsets bottomNavigationEdgeInsets =
+    const EdgeInsets.only(left: 12, right: 12, bottom: 12);
