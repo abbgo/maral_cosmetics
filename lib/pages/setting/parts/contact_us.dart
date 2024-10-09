232415d64d93aec89dbbf5a0ddd1756b05f8f4d5
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-09 18:36:21 +0500

    fix: update ContactUs part widget

--- lib/pages/setting/parts/contact_us.dart
+++ lib/pages/setting/parts/contact_us.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:maral_cosmetics/helpers/methods/bottom_sheets.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
+import 'package:maral_cosmetics/styles/style_borders.dart';
 
 class ContactUs extends StatelessWidget {
   const ContactUs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: defaultLightColor,
-      child: ListTile(
-        onTap: () => showPhonesBottomSheet(context),
-        leading: Icon(Icons.support_agent_outlined, color: defaultColor),
-        title: Text(
-          'Habarlasmak',
-          style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-        ),
+    return ListTile(
+      shape: bottomBorder,
+      onTap: () => showPhonesBottomSheet(context),
+      leading: Icon(Icons.support_agent_outlined, color: defaultColor),
+      title: Text(
+        'Habarlasmak',
+        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
       ),
     );
   }
