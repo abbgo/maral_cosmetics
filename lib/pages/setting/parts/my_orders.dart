04af709c07e9b002c84e2bf7817855c21d630d9a
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 20:36:32 +0500

    fix: update MyOrders part widget

--- lib/pages/setting/parts/my_orders.dart
+++ lib/pages/setting/parts/my_orders.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/providers/local_storadge.dart';
 import 'package:maral_cosmetics/styles/colors.dart';
 import 'package:maral_cosmetics/styles/style_borders.dart';
 
-class MyOrders extends StatelessWidget {
+class MyOrders extends ConsumerWidget {
   const MyOrders({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      shape: bottomBorder,
-      leading: Icon(Icons.local_mall_outlined, color: defaultColor),
-      title: Text(
-        'Menin sargytlarym',
-        style: TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
-      ),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    String accessToken = ref.watch(accessTokenProvider);
+
+    return accessToken.isEmpty
+        ? const SizedBox.shrink()
+        : ListTile(
+            shape: bottomBorder,
+            leading: Icon(Icons.local_mall_outlined, color: defaultColor),
+            title: Text(
+              'Menin sargytlarym',
+              style:
+                  TextStyle(color: defaultColor, fontFamily: 'HeyWowRegular'),
+            ),
+          );
   }
 }
