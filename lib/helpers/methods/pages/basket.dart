351bc847dfcc62bd4fee3d4aac5927e12d9e7a56
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-11 00:11:57 +0500

    feat: add settingPageAppBar method

--- lib/helpers/methods/pages/basket.dart
+++ lib/helpers/methods/pages/basket.dart
@@ -3,20 +3,21 @@ import 'package:maral_cosmetics/styles/colors.dart';
 
 AppBar basketPageAppBar() {
   return AppBar(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Text('Menin sebedim'),
-          Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: defaultLightColor,
-            ),
-            child: Icon(Icons.delete_sweep, color: defaultColor),
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Menin sebedim'),
+        Container(
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: defaultLightColor,
           ),
-        ],
-      ),
-      centerTitle: false,
-      leading: null);
+          child: Icon(Icons.delete_sweep, color: defaultColor),
+        ),
+      ],
+    ),
+    centerTitle: false,
+    leading: null,
+  );
 }
---
+++ lib/helpers/methods/pages/setting.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+AppBar settingPageAppBar() {
+  return AppBar(
+    title: const Text(
+      'Sazlamalar',
+      style: TextStyle(color: Color(0xff824D6A)),
+    ),
+    centerTitle: false,
+    leading: null,
+  );
+}
