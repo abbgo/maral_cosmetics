c69b23c3615ae5c80b1e912a6d6435fbb4091034
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 20:51:36 +0500

    feat: add fetchPhonesProvider provider

--- lib/providers/api/phone.dart
+++ lib/providers/api/phone.dart
@@ -1,4 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/models/phone.dart';
 import 'package:maral_cosmetics/services/api/phone.dart';
 
 final phoneApiProvider = Provider<PhoneApiService>((ref) => PhoneApiService());
+
+var fetchPhonesProvider = FutureProvider.family<ResultPhone, String>(
+  (ref, arg) async {
+    ResultPhone result = ResultPhone.defaultResult();
+
+    try {
+      ResultPhone resultPhone =
+          await ref.read(phoneApiProvider).fetchPhones(arg);
+
+      result = resultPhone;
+    } catch (e) {
+      result = ResultPhone(error: e.toString());
+    }
+
+    return result;
+  },
+);
