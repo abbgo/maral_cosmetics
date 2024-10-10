de7cb62e3fca38f6c9d080955356da2aefe8c590
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 21:37:56 +0500

    feat: add ResultContactPhones part widget

---
+++ lib/pages/parts/contact_phones/parts/result_contact_phones.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maral_cosmetics/helpers/methods/static_methods.dart';
+import 'package:maral_cosmetics/models/phone.dart';
+import 'package:maral_cosmetics/pages/parts/contact_phones/parts/contact_phones_list_tile.dart';
+import 'package:maral_cosmetics/pages/parts/no_result.dart';
+import 'package:maral_cosmetics/providers/api/phone.dart';
+
+class ResultContactPhones extends ConsumerWidget {
+  const ResultContactPhones({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<ResultPhone> resultPhone =
+        ref.watch(fetchPhonesProvider('contact'));
+
+    return resultPhone.when(
+      data: (response) {
+        if (response.phones!.isEmpty) {
+          return const NoResult();
+        }
+
+        List<PhoneModel> phones = response.phones!;
+
+        return ListView.builder(
+          itemCount: phones.length,
+          itemBuilder: (context, index) {
+            PhoneModel phone = phones[index];
+            return ContactPhonesListTile(text: phone.phone);
+          },
+        );
+      },
+      error: (error, stackTrace) => errorMethod(error),
+      loading: () => loadWidget,
+    );
+  }
+}
