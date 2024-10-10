fa1997c3e66d7b2e72b37131d7e4b018c0c2d562
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 23:01:54 +0500

    feat: add minPriceProvider and maxPriceProvider providers

--- lib/providers/parts/product_sort_and_filter.dart
+++ lib/providers/parts/product_sort_and_filter.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var sortProductProvider = StateProvider<String>((ref) => 'all');
+var minPriceProvider = StateProvider<String?>((ref) => null);
+var maxPriceProvider = StateProvider<String?>((ref) => null);
