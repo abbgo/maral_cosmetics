c867e2d4c6d6f3071e5ac37f329acb5e5d730223
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 22:20:46 +0500

    feat: add addOrRemoveFavoriteProvider provider

--- lib/providers/api/favorite.dart
+++ lib/providers/api/favorite.dart
@@ -1,19 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maral_cosmetics/providers/internet.dart';
+import 'package:maral_cosmetics/providers/local_storadge.dart';
 import 'package:maral_cosmetics/services/api/favorite.dart';
 
 final favoriteApiServiceProvider =
     Provider<FavoriteApiService>((ref) => FavoriteApiService());
 
-var logOutUserProvider = FutureProvider.autoDispose.family<bool, BuildContext>(
+var addOrRemoveFavoriteProvider =
+    FutureProvider.autoDispose.family<bool, FavoriteParams>(
   (ref, arg) async {
     bool result = false;
     try {
-      bool hasInternert = await ref.read(checkInternetConnProvider(arg).future);
+      bool hasInternert =
+          await ref.read(checkInternetConnProvider(arg.context).future);
 
       if (hasInternert) {
         String accessToken = await ref.read(accessTokenProvider);
-        result = await ref.read(userApiServiceProvider).logOutUser(accessToken);
+        result = await ref
+            .read(favoriteApiServiceProvider)
+            .addOrRemoveFavorite(accessToken, arg.productID);
       }
     } catch (e) {
       result = false;
