3ed3fc904208e8d3530419614cd3cc27c083688f
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-11 00:10:26 +0500

    feat: add basketPageAppBar mehtod

---
+++ lib/helpers/methods/pages/basket.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:maral_cosmetics/styles/colors.dart';
+
+AppBar basketPageAppBar() {
+  return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text('Menin sebedim'),
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: defaultLightColor,
+            ),
+            child: Icon(Icons.delete_sweep, color: defaultColor),
+          ),
+        ],
+      ),
+      centerTitle: false,
+      leading: null);
+}
